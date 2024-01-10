fed1dd50c0800042d61af1bfccda17c2f3648d3d
Adrien Feubissi Monthe <adrien.feubissimonthe@brp.com>
2024-01-10 10:12:22 -0500

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    /*[HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+
+        return  users;
+    }*/
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return  users;
+    }
+
+    /*[HttpGet("{id}")]  // /api/users/2
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _context.Users.Find(id);
+    }*/
+
+    [HttpGet("{id}")]  // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+
+    //public string? UserName {get; set;}  // defining an optional property with the ? sign
+    public string UserName { get; set; }
+    
+}
