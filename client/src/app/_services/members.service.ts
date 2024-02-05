d8a00fe35c7e9a49d5231bab058f91dc0a1e71d1
Adrien Feubissi Monthe <adrien.feubissimonthe@brp.com>
2024-02-05 10:24:41 -0500

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,21 +2,40 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private Http: HttpClient) { }
 
   getMembers(){
-    return this.Http.get<Member[]>(this.baseUrl + 'users'/*, this.getHttpOptions()*/)
+    if (this.members.length > 0 ) return of(this.members);
+    return this.Http.get<Member[]>(this.baseUrl + 'users'/*, this.getHttpOptions()*/).pipe(
+      map(members => {
+        this.members = members;
+        return members
+      })
+    )
   }
 
   getMember(username: string){
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of(member);
     return this.Http.get<Member>(this.baseUrl + 'users/' + username/*, this.getHttpOptions()*/)
+  }
+
+  updateMember(member: Member){
+    return this.Http.put(this.baseUrl + 'users', member /*, this.getHttpOptions()*/).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}  // javascript spread operator 
+      })
+    )
   }
 
   // getHttpOptions(){
